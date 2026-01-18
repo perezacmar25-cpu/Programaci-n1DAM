f0c3d391b98c6a49680362982748c6b1b5de2894
unknown <perez.acmar25@triana.salesianos.edu>
2026-01-18 15:48:29 +0100

    Ejercicio 01 boletín 2 acabado

---
+++ eclipse-workspace/EjerciciosT4MarioPérez/src/ejercicio01B2/IGestion.java
@@ -0,0 +1,23 @@
+package ejercicio01B2;
+
+public interface IGestion {
+	
+	
+	public int cuentaPrestados (Publicacion [] lista);
+	
+	public int publicacionesAnterioresA(Publicacion []  lista,int annyo);
+		
+		
+		
+	
+		
+		
+		
+		
+	
+	
+	
+	
+	
+
+}
